149f5885a27410d160c04cf63557e87512c30ec0
Kotar Creative <kotarcreative@gmail.com>
2018-09-07 23:10:16 -0600

    Make inputs for profile edits larger.

--- resources/assets/js/components/users/edit/sections/profile.vue
+++ resources/assets/js/components/users/edit/sections/profile.vue
@@ -3,29 +3,29 @@
     <h2>Profile Info</h2>
     <div class="form-group">
       <label for="first-name" class="required-field">First Name</label>
-      <input type="text" class="form-control sm" :class="{ 'has-error': hasError('current_password')}" name="first-name" v-model="form.first_name" :disabled="loading">
+      <input type="text" class="form-control md" :class="{ 'has-error': hasError('current_password')}" name="first-name" v-model="form.first_name" :disabled="loading">
     </div>
     <div class="form-group">
       <label for="last-name">Last Name</label>
-      <input type="text" class="form-control sm" name="last-name" v-model="form.last_name" :disabled="loading">
+      <input type="text" class="form-control md" name="last-name" v-model="form.last_name" :disabled="loading">
     </div>
     <h2>Login Info</h2>
     <div class="form-group">
       <label for="email">Login Email</label>
-      <input type="text" class="form-control sm" name="email" v-model="form.email" :disabled="loading">
+      <input type="text" class="form-control md" name="email" v-model="form.email" :disabled="loading">
     </div>
     <div class="form-group">
       <label for="password">Password</label>
-      <input type="password" class="form-control sm" name="password" v-model="form.password" :disabled="loading">
+      <input type="password" class="form-control md" name="password" v-model="form.password" :disabled="loading">
       <div class="input-notice">Leave it blank if you don't want to change it.</div>
     </div>
     <div class="form-group">
       <label for="password_confirmation">Password Confirmation</label>
-      <input type="password" class="form-control sm" name="password_confirmation" v-model="form.password_confirmation" :disabled="loading">
+      <input type="password" class="form-control md" name="password_confirmation" v-model="form.password_confirmation" :disabled="loading">
     </div>
     <div class="form-group">
       <label for="current_password" class="required-field">Current Password</label>
-      <input type="password" class="form-control sm" :class="{ 'has-error': hasError('current_password')}" name="current_password" v-model="form.current_password" :disabled="loading">
+      <input type="password" class="form-control md" :class="{ 'has-error': hasError('current_password')}" name="current_password" v-model="form.current_password" :disabled="loading">
       <div class="input-notice">We will need your current password to confirm you changes.</div>
     </div>
     <div class="form-errors" v-if="hasErrors()">
